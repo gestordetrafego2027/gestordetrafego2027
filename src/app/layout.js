0a20442b085782d594a17e10c2d32910f4a5f899
Henry Almeida <gestordetrafego2027@gmail.com>
2026-04-20 10:28:12 -0300

    feat: tipografia Roc Grotesk

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,4 @@
-import { Syne } from 'next/font/google';
 import "./globals.css";
-
-const syne = Syne({
-  subsets: ['latin'],
-  weight: ['400', '500', '600', '700', '800'],
-  variable: '--font-headline'
-});
 
 export const metadata = {
   title: "House Mazzutti | Branding Estratégico e Posicionamento de Marca",
@@ -14,7 +7,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="pt-BR" className={`${syne.variable}`}>
+    <html lang="pt-BR" className="font-body">
       <head>
         <link
           href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=swap"
