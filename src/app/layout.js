49de6b8c89118205919e09baa8ffc03d1020a3ad
Henry Almeida <gestordetrafego2027@gmail.com>
2026-04-22 08:28:44 -0300

    feat: Roc Grotesk completa

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,14 +7,14 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="pt-BR" className="font-body">
+    <html lang="pt-BR">
       <head>
         <link
           href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=swap"
           rel="stylesheet"
         />
       </head>
-      <body className="antialiased">
+      <body className="font-body">
         {children}
       </body>
     </html>
