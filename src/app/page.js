e210712c5e866648d52c810dbafe98e0df1450c6
ANGELO MAZZUTTI <gestordetrafego2027@gmail.com>
2026-05-08 00:04:05 -0300

    fix: atualiza imagem do banner home e corrige caminho no código

--- src/app/page.js
+++ src/app/page.js
@@ -150,7 +150,7 @@ export default function Home() {
                     style={{ height: "105vh" }}
                 >
                     <div className="absolute inset-0 z-0">
-                        {['/images/home/banner-1.jpg','/images/home/banner-2.jpg','/images/home/banner-3.jpg'].map((src, i) => (
+                        {['/images/home/banner-1.png','/images/home/banner-2.jpg','/images/home/banner-3.jpg'].map((src, i) => (
                             <div key={i} className="absolute inset-0 transition-opacity duration-1000" style={{opacity: currentHeroSlide === i ? 1 : 0}}>
                                 <img src={src} className="w-full h-full object-cover object-top" alt="" />
                                 <div className="absolute inset-0 bg-black/20"></div>
